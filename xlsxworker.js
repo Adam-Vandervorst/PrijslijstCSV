5a04b7f3181fe01dacc91ec271f4084a84aa722a
Adam-Vandervorst <adam.vandervorst@gmail.com>
2021-11-15 21:27:53 +0100

    Initial commit

---
+++ xlsxworker.js
@@ -0,0 +1,11 @@
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.6/shim.min.js",
+              "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.6/xlsx.full.min.js");
+postMessage({t:"ready"});
+
+onmessage = function (evt) {
+  var v;
+  try {
+    v = XLSX.read(evt.data.d, {type: evt.data.b});
+postMessage({t:"xlsx", d:JSON.stringify(v)});
+  } catch(e) { postMessage({t:"e",d:e.stack||e}); }
+};
