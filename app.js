81a9fcb8bfdfad40dc120a4fdb89ae4173f18922
Adam-Vandervorst <adam.vandervorst@gmail.com>
2021-11-15 21:45:11 +0100

    minor code cleanup

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ function download(filename, text) {
 }
 
 
-var X = XLSX;
-var XW = {
-	msg: 'xlsx',
-	worker: './xlsxworker.js'
+let X = XLSX;
+let XW = {
+    msg: 'xlsx',
+    worker: './xlsxworker.js'
 };
 
-var global_wb;
+let global_wb;
 
 function process_wb(wb) {
     if (!wb) return;
@@ -28,13 +28,16 @@ function process_wb(wb) {
     let list = [["CATEGORIE", "VOLNAAM", "BRUTOPRIJS"]];
 
     while (passed < max_passed) {
-        cell = sheet[X.utils.encode_cell({c:0, r:row_index})];
+        cell = sheet[X.utils.encode_cell({c: 0, r: row_index})];
 
         row_index++;
-        if (!cell) {passed++; continue}
+        if (!cell) {
+            passed++;
+            continue
+        }
         passed = 0;
 
-        next_cell = sheet[X.utils.encode_cell({c:0, r:row_index})];
+        next_cell = sheet[X.utils.encode_cell({c: 0, r: row_index})];
 
         if (next_cell) { // cell is category
             category = cell.v;
@@ -42,17 +45,18 @@ function process_wb(wb) {
         } else { // cell is a product group
             try {
                 while (true) {
-                    let product_cell = sheet[X.utils.encode_cell({c:1, r:row_index})];
+                    let product_cell = sheet[X.utils.encode_cell({c: 1, r: row_index})];
                     if (!product_cell) break;
-                    let price_cell = sheet[X.utils.encode_cell({c:2, r:row_index})];
-                    let price =  (price_cell && !isNaN(price_cell.v)) ? price_cell.v : 0;
+                    let price_cell = sheet[X.utils.encode_cell({c: 2, r: row_index})];
+                    let price = (price_cell && !isNaN(price_cell.v)) ? price_cell.v : 0;
                     list.push([category, product_cell.v, price.toFixed(2)]);
                     row_index++;
                 }
             } catch (err) {
-                console.log({loc: row_index, cat: category, cell: cell, next_cell: next_cell,
-                    r1: sheet[X.utils.encode_cell({c:1, r:row_index})],
-                    r2: sheet[X.utils.encode_cell({c:2, r:row_index})],
+                console.log({
+                    loc: row_index, cat: category, cell: cell, next_cell: next_cell,
+                    r1: sheet[X.utils.encode_cell({c: 1, r: row_index})],
+                    r2: sheet[X.utils.encode_cell({c: 2, r: row_index})],
                 })
             }
         }
@@ -61,52 +65,58 @@ function process_wb(wb) {
     download("price_list_export.csv", X.utils.sheet_to_csv(X.utils.aoa_to_sheet(list), {FS: ';'}))
 }
 
-var b64it = window.b64it = (function() {
-	var tarea = document.getElementById('b64data');
-	return function b64it() {
-		if(typeof console !== 'undefined') console.log("onload", new Date());
-		var wb = X.read(tarea.value, {type:'base64', WTF:false});
-		process_wb(wb);
-	};
+const b64it = window.b64it = (function () {
+    const tarea = document.getElementById('b64data');
+    return function () {
+        if (typeof console !== 'undefined') console.log("onload", new Date());
+        const wb = X.read(tarea.value, {type: 'base64', WTF: false});
+        process_wb(wb);
+    };
 })();
 
-var do_file = (function() {
-	var rABS = typeof FileReader !== "undefined" && (FileReader.prototype||{}).readAsBinaryString;
-	var domrabs = document.getElementsByName("userabs")[0];
-	if(!rABS) domrabs.disabled = !(domrabs.checked = false);
+const do_file = (function () {
+    let rABS = typeof FileReader !== "undefined" && (FileReader.prototype || {}).readAsBinaryString;
+    let domrabs = document.getElementsByName("userabs")[0];
+    if (!rABS) domrabs.disabled = !(domrabs.checked = false);
 
-	var use_worker = typeof Worker !== 'undefined';
-	var domwork = document.getElementsByName("useworker")[0];
-	if(!use_worker) domwork.disabled = !(domwork.checked = false);
+    let use_worker = typeof Worker !== 'undefined';
+    let domwork = document.getElementsByName("useworker")[0];
+    if (!use_worker) domwork.disabled = !(domwork.checked = false);
 
-	var xw = function xw(data, cb) {
-		var worker = new Worker(XW.worker);
-		worker.onmessage = function(e) {
-			switch(e.data.t) {
-				case 'ready': break;
-				case 'e': console.error(e.data.d); break;
-				case XW.msg: cb(JSON.parse(e.data.d)); break;
-			}
-		};
-		worker.postMessage({d:data,b:rABS?'binary':'array'});
-	};
+    const xw = function xw(data, cb) {
+        let worker = new Worker(XW.worker);
+        worker.onmessage = function (e) {
+            switch (e.data.t) {
+                case 'ready':
+                    break;
+                case 'e':
+                    console.error(e.data.d);
+                    break;
+                case XW.msg:
+                    cb(JSON.parse(e.data.d));
+                    break;
+            }
+        };
+        worker.postMessage({d: data, b: rABS ? 'binary' : 'array'});
+    };
 
-	return function do_file(files) {
-		rABS = true;
-		use_worker = true;
-		var f = files[0];
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			console.log("onload", new Date(), rABS, use_worker);
-			var data = e.target.result;
-			if(use_worker) xw(data, process_wb);
-			else process_wb(X.read(data, {type: rABS ? 'binary' : 'array'}));
-		};
-		if(rABS) reader.readAsBinaryString(f);
-	};
+    return function (files) {
+        rABS = true;
+        use_worker = true;
+        const f = files[0];
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            console.log("onload", new Date(), rABS, use_worker);
+            const data = e.target.result;
+            if (use_worker) xw(data, process_wb);
+            else process_wb(X.read(data, {type: rABS ? 'binary' : 'array'}));
+        };
+        if (rABS) reader.readAsBinaryString(f);
+    };
 })();
 
 const drop = document.getElementById('drop');
+
 function handleDrop(e) {
     e.stopPropagation();
     e.preventDefault();
@@ -124,4 +134,4 @@ drop.addEventListener('dragover', handleDragover, false);
 drop.addEventListener('drop', handleDrop, false);
 
 const xlf = document.getElementById('xlf');
-xlf.addEventListener('change', (e) => do_file(e.target.files), false);
+xlf.addEventListener('change', function (e) {do_file(e.target.files)}, false);
--- xlsxworker.js
+++ xlsxworker.js
@@ -1,11 +1,12 @@
 importScripts("https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.6/shim.min.js",
               "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.6/xlsx.full.min.js");
-postMessage({t:"ready"});
+postMessage({t: "ready"});
 
 onmessage = function (evt) {
-  var v;
-  try {
-    v = XLSX.read(evt.data.d, {type: evt.data.b});
-postMessage({t:"xlsx", d:JSON.stringify(v)});
-  } catch(e) { postMessage({t:"e",d:e.stack||e}); }
+    try {
+        let v = XLSX.read(evt.data.d, {type: evt.data.b});
+        postMessage({t: "xlsx", d: JSON.stringify(v)});
+    } catch (e) {
+        postMessage({t: "e", d: e.stack || e});
+    }
 };
